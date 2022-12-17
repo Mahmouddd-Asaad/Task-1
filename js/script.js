a727ab2fe1fa74d86d281ecf05b7735538735462
Mahmoudd-Asaad <mahmouddd.alasaad.12@gmail.com>
2022-12-17 23:30:42 +0300

    upload files

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ $(function () {
     $('.logo-click').on('click',function(){
         $('.product-desktop-main-container').hide();
         $('.homepage-desktop').fadeIn();
-        console.log('homepage-desktop');
     });
 
     $('.category-menu-desktop-hermes-theme a').on('click',function(){
         $('.homepage-desktop').hide();
         $('.product-desktop-main-container').hide();
         $('.all-product' +' '+ $(this).data('content')).fadeIn();
+        console.log($(this).data('content'));
+    });
 
+    $('.desktop-homepage-categories .list .item-wrapper').on('click',function(){
+        $('.homepage-desktop').hide();
+        $('.product-desktop-main-container').hide();
+        $('.all-product' +' '+ $(this).data('content')).fadeIn();
     });
 });
