45500f0dc93a4f29249b7f63eaf420f65cd165bc
Mahmoudd-Asaad <mahmouddd.alasaad.12@gmail.com>
2022-12-26 23:18:44 +0300

    main content mobile programming finished

--- js/script.js
+++ js/script.js
@@ -23,4 +23,34 @@ $(function () {
         $('.btn-group .switch span').removeClass('active');
         $(this).addClass('active');
     });
+
+
+    $('.mobile-homepage-categories .list .item-wrapper').on('click',function(){
+        $('.homepage-mobile').hide();
+        $('.main-content-mobile').fadeIn();
+    });
+
+    $('.main-content-mobile .mobile-header .logo').on('click',function(){
+        $('.main-content-mobile').hide();
+        $('.homepage-mobile').fadeIn();
+    });
+
+    $('.swiper-wrapper .swiper-slide > a').on('click',function(e){
+        e.preventDefault();
+        var dtop = $('#'+$(this).data('content')).css('top');
+        $('.swiper-wrapper .swiper-slide > a').removeClass('active');
+        $(this).addClass('active');
+        $('html,body,.products-list-container').animate({
+            scrollTop: dtop
+        },600);
+    });
+
+    $('.products-list-container').scroll(function(){
+        $('.product-index').each(function(){
+            var ddtop = $(this).css('top')
+            if($('.main-products-wrapper').scrollTop() > ddtop){
+                console.log('true')
+            }else{console.log($(this).css('top'))}
+        });
+    });
 });
